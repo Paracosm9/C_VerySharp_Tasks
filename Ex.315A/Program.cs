1a8210b133cd0768cfd9849c2fa88f5c18fd91a9
Paracosm9 <lemniskata9@gmail.com>
2025-05-09 16:03:17 +0300

    First task ever in C#

--- Ex.315A/Program.cs
+++ Ex.315A/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+int randomNumber = new Random().Next(1,1000);
+int amountOfAttempts = 0;
+
+Console.WriteLine("Enter the number to guess! ");
+int start = Int32.Parse(Console.ReadLine());
+while (start != randomNumber)
+{
+    if (start > randomNumber)
+    {
+        Console.WriteLine($"{start} is bigger!");
+    }
+
+    else if (start < randomNumber)
+    {
+        Console.WriteLine($"{start} is smaller!");
+    }
+    amountOfAttempts++;
+    start = Int32.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"It took {amountOfAttempts} attempts to guess number - {start}!");
